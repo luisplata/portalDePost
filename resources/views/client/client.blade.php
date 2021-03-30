7665795c50808b9b4be2efbd48eae877566a7a1a
Luis Plata <www.luisplata@gmail.com>
2021-03-29 22:55:45 -0500

    the title of page is corrected

--- resources/views/client/client.blade.php
+++ resources/views/client/client.blade.php
@@ -20,7 +20,7 @@
         gtag('config', '{{ env('CLAVE_DE_GOOGLE_ANALYTICS') }}');
 
     </script>
-    <title>Generic - Editorial by HTML5 UP</title>
+    <title>{{ env('APP_NAME') }}</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
     <link rel="stylesheet" href="{{ url('') }}/assets/css/main.css?v{{ date('Ymdhs') }}" />
