b2fb61a742ed3baadbd8b90fe94ddcc702946b51
Luis Plata <www.luisplata@gmail.com>
2021-04-03 09:14:59 -0500

    the products is added in admin page

--- resources/views/admin/producto/dashboard.blade.php
+++ resources/views/admin/producto/dashboard.blade.php
@@ -6,44 +6,36 @@
         <li><a href="{{url('admin/producto/create')}}"><i class="fa fa-plus"></i> Crear</a></li>
     </ul>
 </div>
-@foreach($productos as $producto)
-<div class="col-md-4 col-sm-4 col-xs-12 profile_details">
-    <div class="well profile_view">
-        <div class="col-sm-12">
-            <div class="left col-xs-7">
-                <h2>{{$producto->nombre}}</h2>
-                <img  class="img-thumbnail"  src="{{$producto->imagen}}" />
-                <ul class="list-unstyled">
-                    <li><i class="fa fa-check"></i> Estado: {{$producto->estado == 1 ? "Activo":"Inactivo"}}</li>
-                    <li><i class="fa fa-phone"></i> {{$producto->categoria->nombre}}</li>
-                    <li><i class="fa fa-phone"></i>Publicado?: {{strtotime($producto->publication_date) <= strtotime(date("Y-m-d H:i:s"))?"Si":"No"}}</li>
-                </ul>
-            </div>
-            <div class="right col-xs-5 text-center">
-                <!--<img src="images/img.jpg" alt="" class="img-circle img-responsive">-->
-            </div>
-        </div>
-        <div class="col-xs-12 bottom text-center">
-            <div class="col-xs-12 col-sm-6 emphasis hidden">
-                <p class="ratings">
-                    <a>4.0</a>
-                    <a href="#"><span class="fa fa-star"></span></a>
-                    <a href="#"><span class="fa fa-star"></span></a>
-                    <a href="#"><span class="fa fa-star"></span></a>
-                    <a href="#"><span class="fa fa-star"></span></a>
-                    <a href="#"><span class="fa fa-star-o"></span></a>
-                </p>
-            </div>
-            <div class="col-xs-12 col-sm-12 emphasis">
-                <a class="btn btn-primary" href="{{url("admin/producto/$producto->id")}}"><i class="fa fa-eye"></i> Ver</a>
-                <a class="btn btn-primary" href="{{url("admin/producto/$producto->id/edit")}}"><i class="fa fa-edit"></i> Editar</a>
-                {{Form::open(["url"=>"admin/producto/$producto->id","method"=>"DELETE"])}}
-                <button type="submit" class="btn btn-primary"><i class="fa fa-ellipsis-h"></i> Eliminar</button>
-                {{Form::close()}}
-            </div>
-        </div>
-    </div>
-</div>
-@endforeach
-{{ $productos->links() }}
+
+<table class="table table-striped table-hover" id="tableData">
+    <thead>
+      <tr>
+        <th scope="col">Nombre</th>
+        <th scope="col">Esta Publicado?</th>
+        <th scope="col">Editar</th>
+        <th scope="col">Eliminar</th>
+      </tr>
+    </thead>
+    <tbody>
+        @foreach($productos as $producto)
+      <tr>
+        <th>{{$producto->nombre}}</th>
+        <td>{{strtotime($producto->publication_date) <= strtotime(date("Y-m-d H:i:s"))?"Si":"No"}}</td>
+        <td><a class="btn btn-primary" href="{{url("admin/producto/$producto->id/edit")}}"><i class="fa fa-edit"></i> Editar</a></td>
+        <td>{{Form::open(["url"=>"admin/producto/$producto->id","method"=>"DELETE"])}}
+            <button type="submit" class="btn btn-primary"><i class="fa fa-ellipsis-h"></i> Eliminar</button>
+            {{Form::close()}}</td>
+      </tr>
+      @endforeach
+    </tbody>
+</table>
+
+<script src="//code.jquery.com/jquery-3.5.1.js"></script>
+<link rel="stylesheet" href="//cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css">
+<br><script src = "//cdn.datatables.net/1.10.18/js/jquery.dataTables.min.js" defer ></script>
+<script>
+    $(document).ready( function () {
+        $('#tableData').DataTable();
+    } );
+</script>
 @endsection
