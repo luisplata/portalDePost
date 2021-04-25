ea933454a9db833e176344bc858cb1036e795edb
Luis Plata <www.luisplata@gmail.com>
2021-04-24 21:53:45 -0500

    feeling up x2

--- resources/views/admin/producto/edit.blade.php
+++ resources/views/admin/producto/edit.blade.php
@@ -26,10 +26,10 @@
 </div>
 <div class="clearfix"></div>
 <div class="form-group">
-    <label class="control-label col-md-3 col-sm-3 col-xs-12" for="imagen">Imagen Source <span class="required">*</span>
+    <label class="control-label col-md-3 col-sm-3 col-xs-12" for="imagen">Imagen Source
     </label>
     <div class="col-md-6 col-sm-6 col-xs-12">
-        <input type="text" id="imagen" name="imagen" required="required" value="{{$producto->imagen}}" class="form-control col-md-7 col-xs-12">
+        <input type="text" id="imagen" name="imagen" value="{{$producto->imagen}}" class="form-control col-md-7 col-xs-12">
     </div>
 </div>
 
