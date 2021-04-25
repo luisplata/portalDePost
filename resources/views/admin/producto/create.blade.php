ab73cf64f1319b4d43f868858c2af8e893cf0e8e
Luis Plata <www.luisplata@gmail.com>
2021-04-24 21:51:35 -0500

    feeling up

--- resources/views/admin/producto/create.blade.php
+++ resources/views/admin/producto/create.blade.php
@@ -12,10 +12,10 @@
 </div>
 <div class="clearfix"></div>
 <div class="form-group">
-    <label class="control-label col-md-3 col-sm-3 col-xs-12" for="last-name">Imagen Source <span class="required">*</span>
+    <label class="control-label col-md-3 col-sm-3 col-xs-12" for="last-name">Imagen Source
     </label>
     <div class="col-md-6 col-sm-6 col-xs-12">
-        <input type="text" id="last-name" name="url" required="required" value="#" class="form-control col-md-7 col-xs-12">
+        <input type="text" id="last-name" name="url" value="#" class="form-control col-md-7 col-xs-12">
     </div>
 </div>
 <div class="clearfix"></div>
