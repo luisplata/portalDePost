b946ac59645a94b49ca4e68a8b2735dcf0411b5e
PeryLoth <www.luisplata@gmail.com>
2022-01-10 09:32:16 -0500

    solucionando problema en visualizacion de cartas de streams

--- resources/views/ScrollInfiniteStream.blade.php
+++ resources/views/ScrollInfiniteStream.blade.php
@@ -2,7 +2,7 @@
     @foreach ($packs as $stream)
         <div class="col">
             <a href="{{ env('HOME') }}PPV/{{ $stream->ConvertNameNormalToUrl() }}">
-                <div class="card bg-dark text-center">
+                <div class="card bg-dark text-center footer-widget">
                     <img src="{{ $stream->imagen }}" class="card-img">
                     <div class="card-body">
                         <p class="card-text"></p><h2 class="card-title">{{ $stream->nombre }}</h2><p></p>
