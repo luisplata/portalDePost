47f4b1ec87ce3f130f0df16da8690940dd2e591d
PeryLoth <www.luisplata@gmail.com>
2025-05-21 01:28:21 -0500

    +

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,7 +20,6 @@ class Kernel extends HttpKernel {
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
-        \Fruitcake\Cors\HandleCors::class,
     ];
 
     /**
@@ -42,6 +41,7 @@ class Kernel extends HttpKernel {
         'api' => [
             'throttle:60,1',
             'bindings',
+            \Fruitcake\Cors\HandleCors::class,
         ],
     ];
 
