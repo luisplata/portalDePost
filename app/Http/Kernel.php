af2743b1821af565b3098ab18c2b1ddcc952f62c
PeryLoth <www.luisplata@gmail.com>
2025-05-21 01:32:23 -0500

    ++

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends HttpKernel {
      * @var array
      */
     protected $middleware = [
+        \Fruitcake\Cors\HandleCors::class,
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
@@ -41,7 +42,6 @@ class Kernel extends HttpKernel {
         'api' => [
             'throttle:60,1',
             'bindings',
-            \Fruitcake\Cors\HandleCors::class,
         ],
     ];
 
